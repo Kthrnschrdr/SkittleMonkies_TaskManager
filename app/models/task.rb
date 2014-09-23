30079ec97d951cca6b8a9e2a95bf2f906efd8138
Kathryn Schroeder <kthrnschrdr@gmail.com>
2014-09-22 22:13:19 -0500

    added virtual attribute to create a new category within the new task form

--- app/models/task.rb
+++ app/models/task.rb
@@ -2,18 +2,16 @@ class Task < ActiveRecord::Base
   
   include PublicActivity::Common
   # tracked owner: ->(controller, model) { controller && controller.current_user }
-  attr_accessible :description, :status, :task_name, :user_id, :email, :project_id, :category_id
+  attr_accessible :description, :status, :task_name, :user_id, :email, :project_id, :category_id, :new_category
   belongs_to :project
   belongs_to :user
   belongs_to :category  
   has_many :comments
   
-  # def send_email(user, task)
- #    if self.email == ""
- #      puts "Email field was empty"
- #    else
- #      UserMailer.receive_task(@task).deliver
- #    end
- #  end
+  attr_accessor :new_category
+  
+  def create_category
+    self.category = Category.create!(name: new_category) if new_category.present?
+  end
 
 end
